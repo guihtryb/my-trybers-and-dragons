95956285d674dae66a36c771273ce0e28302b8f2
Guilherme Viana <guilhermehviana01@gmail.com>
2022-04-12 19:29:50 -0300

    Add Archetype Interface

---
+++ src/Interfaces/IArchetype.ts
@@ -0,0 +1,5 @@
+export default interface IArchetype {
+  name: string;
+  special: number;
+  cost: number;
+}
--- src/Interfaces/index.ts
+++ src/Interfaces/index.ts
@@ -1,8 +1,12 @@
 import IRace from './IRace';
 import ICreature from './ICreature';
+import Energy from '../Energy';
+import IArchetype from './IArchetype';
 
 export default IRace;
 
 export {
   ICreature,
+  Energy,
+  IArchetype,
 };
