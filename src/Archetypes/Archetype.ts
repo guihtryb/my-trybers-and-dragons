6d6abbeb4f64280c83d338965b523fb6d6af1a26
Guilherme Viana <guilhermehviana01@gmail.com>
2022-04-12 19:31:55 -0300

    Add abstract class Archetype

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,32 @@
+import { EnergyType } from '../Energy';
+import { IArchetype } from '../Interfaces';
+
+export default abstract class Archetype implements IArchetype {
+  private readonly _special: number;
+  private readonly _cost: number;
+
+  constructor(
+    private readonly _name: string,
+  ) {
+    this._special = 0;
+    this._cost = 0;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get special(): number {
+    return this._special;
+  }
+
+  get cost(): number {
+    return this._cost;
+  }
+
+  static createdArchetypeInstances():number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get energyType(): EnergyType;
+}
