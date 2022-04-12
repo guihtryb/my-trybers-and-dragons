44bac4304f6360696c47c711a92131e8c2a8a954
Guilherme Viana <guilhermehviana01@gmail.com>
2022-04-12 17:41:37 -0300

    Add Elf race

---
+++ src/Races/Elf.ts
@@ -0,0 +1,32 @@
+import { ICreature } from '../Interfaces';
+import Race from './Race';
+
+export default class Elf extends Race implements ICreature {
+  protected _maxLifePoints = 99;
+  private static elfUnits = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    Elf.increaseElfQuantity();
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  private static increaseElfQuantity(): void {
+    Elf.validateQuantity(Elf.elfUnits, 40);
+
+    Elf.elfUnits += 1;
+  }
+
+  static createdRacesInstances(): number {
+    return Elf.elfUnits;
+  }
+
+  private static validateQuantity(currQuantity:number, maxQuantity:number) {
+    if (currQuantity === maxQuantity) {
+      throw new Error('Elf race has reached maximum number');
+    }
+  }
+}
