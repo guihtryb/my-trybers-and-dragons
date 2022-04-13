49825de47ed89d4b3cc840c67b05dd5de86f6b88
Guilherme Viana <guilhermehviana01@gmail.com>
2022-04-13 12:42:53 -0300

    Create Warrior Archetype

---
+++ src/Archetypes/Warrior.ts
@@ -0,0 +1,35 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Warrior extends Archetype {
+  private readonly _energyType: EnergyType;
+  private static warriorUnits = 0;
+
+  constructor(name: string) {
+    super(name);
+
+    Warrior.increaseWarriorUnits();
+
+    this._energyType = 'stamina';
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  static createdArchetypeInstances(): number {
+    return Warrior.warriorUnits;
+  }
+
+  static increaseWarriorUnits() {
+    Warrior.validateQuantity(Warrior.warriorUnits, 30);
+
+    Warrior.warriorUnits += 1;
+  }
+
+  private static validateQuantity(currQuantity:number, maxQuantity:number) {
+    if (currQuantity === maxQuantity) {
+      throw new Error('Warrior archetype has reached maximum quantity');
+    }
+  }
+}
