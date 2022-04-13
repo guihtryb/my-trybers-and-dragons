30bcb3dff477783299036ba2d6692e8c64c5006e
Guilherme Viana <guilhermehviana01@gmail.com>
2022-04-13 12:05:46 -0300

    Create Mage Archetype

---
+++ src/Archetypes/Mage.ts
@@ -0,0 +1,33 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Mage extends Archetype {
+  private readonly _energyType: EnergyType;
+  private static mageUnits = 0;
+
+  constructor(name: string) {
+    super(name);
+    Mage.increaseMageUnits();
+    this._energyType = 'mana';
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  static createdArchetypeInstances(): number {
+    return Mage.mageUnits;
+  }
+
+  static increaseMageUnits() {
+    Mage.validateQuantity(Mage.mageUnits, 30);
+
+    Mage.mageUnits += 1;
+  }
+
+  private static validateQuantity(currQuantity:number, maxQuantity:number) {
+    if (currQuantity === maxQuantity) {
+      throw new Error('Mage archetype has reached maximum quantity');
+    }
+  }
+}
